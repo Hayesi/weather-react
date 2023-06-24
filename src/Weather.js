7d5e83341f451a9b3c4aec3ce2b9564b938cf379
Hayesi <122321074+Hayesi@users.noreply.github.com>
2023-06-24 09:00:05 +0100

    Created weather.js folder

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+
+export default function Weather() {
+let apikey =  let apikey = "4a8afcfcb072727adab5b1a90338a6a7";
+let apiUrl =  `https://api.openweathermap.org/data/2.5/forecast?q=${
+    props.city
+}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+return <h2>Hello from Weather</h2>;
+}
